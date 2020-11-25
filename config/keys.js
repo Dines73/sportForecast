340e090143d0345fba6f02df0a8b5d78c9cb7622
edin <edo.truman@gmail.com>
2020-11-25 10:57:08 +0100

    keys changed

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,8 @@
-dbPassword = 'mongodb+srv://Edin:'+ encodeURIComponent('stelium1102') + '@node-rest-shop-jsubz.mongodb.net/test?retryWrites=true';
-
+dbPassword =
+  "mongodb+srv://Name:" +
+  encodeURIComponent("password") +
+  "@mongodb-name.mongodb.net/test?retryWrites=true"
 
 module.exports = {
-    mongoURI: dbPassword
-};
+  mongoURI: dbPassword
+}
