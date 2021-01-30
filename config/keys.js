489a8e3d8d40f0187771f28036ee9336d011ebd5
edin <edo.truman@gmail.com>
2021-01-30 17:08:09 +0100

    screen shot

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 dbPassword =
   "mongodb+srv://Name:" +
   encodeURIComponent("password") +
-  "@mongo-name-jsubz.mongodb.net/test?retryWrites=true"
+  "@node-rest-shop-jsubz.mongodb.net/test?retryWrites=true"
 module.exports = {
   mongoURI: dbPassword
 }
